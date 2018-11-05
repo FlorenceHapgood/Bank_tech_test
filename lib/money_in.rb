2d74102f8c57728df508ca444dd63cff84fd4d5c
Florence Hapgood <flohapgood@icloud.com>
2018-11-05 14:52:52 +0000

    Test passing

--- lib/money_in.rb
+++ lib/money_in.rb
@@ -1,9 +1,15 @@
+require "date"
+
 class Money_In
+
+  attr_reader :amount
 
   def initialize (amount)
     @amount = amount
   end
 
-
+  def dep_record
+    { Deposit_amount: amount, Date: Date.today }
+  end
 
 end

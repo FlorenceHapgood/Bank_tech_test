2e930f77ca98bca5a39fcc515b9eec37d8d976d5
Florence Hapgood <flohapgood@icloud.com>
2018-11-05 11:47:45 +0000

    Test written for the desposit method, to check that it adds the amount deposited to the balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,13 @@
 require "account"
 
 describe Account do
-  
+
+  let(:account) { described_class.new }
+
+  describe "#deposit" do
+    it "adds the amount deposited to the balance" do
+      account.deposit(400)
+      expect(account.balance).to eq(400)
+    end
+  end
 end

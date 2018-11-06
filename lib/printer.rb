e869af202aa5fc02ed89b0e976d6e15f30b521cb
Florence Hapgood <flohapgood@icloud.com>
2018-11-06 10:48:36 +0000

    Test passed and refactored into reverse method

--- lib/printer.rb
+++ lib/printer.rb
@@ -9,10 +9,14 @@ attr_reader :history
 
   def print_statement
     puts "date || credit || debit || balance"
+    reverse
     @history.each { |hash|
     puts hash[:Date].to_s + " || " + hash[:Credit].to_s + " || " + hash[:Debit].to_s + " || " + hash[:Balance].to_s
     }
   end
 
+  def reverse
+    @history.reverse!
+  end
 
 end

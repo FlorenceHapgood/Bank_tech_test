b8a5e94b561b545a42448321dd6231ed63ad6f3b
Florence Hapgood <flohapgood@icloud.com>
2018-11-06 12:04:19 +0000

    Timecop added to feature test
    So that the tests won't fail tomorrow

--- spec/features/user_can_use_account_spec.rb
+++ spec/features/user_can_use_account_spec.rb
@@ -1,9 +1,11 @@
 require 'account'
 require 'printer'
+require 'timecop'
 
 describe "Bank" do
   it "can print out a correct statement" do
     account = Account.new
+    Timecop.freeze(Date.parse('6 November 2018'))
     account.deposit(1000.00)
     account.deposit(2000.00)
     account.withdraw(500.00)
